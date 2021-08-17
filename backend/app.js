ef41c55330726b0044216868cd8cdea433db98fa
Faiza Hamidi <hamidi.faiza@gmail.com>
2021-08-17 14:56:37 +0200

    Mise à jour des variables d'environnement

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ mongoose
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", process.env.ALLOW_ORIGIN);
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./app");
 
-app.set("port", 3000);
+app.set("port", process.env.PORT);
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(process.env.PORT);
